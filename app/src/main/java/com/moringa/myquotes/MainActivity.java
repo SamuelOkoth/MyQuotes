9040ba6059e5c5c5c4ff0877dfb91b7034b4ef07
SamuelOkoth <okothsamuel78@gmail.com>
2019-08-26 13:13:11 +0300

    this commit adds an inntent

--- app/src/main/java/com/moringa/myquotes/MainActivity.java
+++ app/src/main/java/com/moringa/myquotes/MainActivity.java
@@ -6,22 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindQuotesButton;
+    private EditText mQuotesEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindQuotesButton = (Button) findViewById(R.id.FindQuotesButton);
+        mQuotesEditText = (EditText) findViewById(R.id.QuotesEditText);
         mFindQuotesButton = (Button) findViewById(R.id.FindQuotesButton);
         mFindQuotesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Welcome to The Quotes World", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, QuotesActivity.class);
+                intent.putExtra("intent", intent);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/moringa/myquotes/QuotesActivity.java
+++ app/src/main/java/com/moringa/myquotes/QuotesActivity.java
@@ -2,13 +2,28 @@ package com.moringa.myquotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class QuotesActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
+        TextView mQuotesEditText = (TextView) findViewById(R.id.QuotesEditText);
+        Intent intent = getIntent();
+        String Quotes = intent.getStringExtra("Quotes");
+
+
+
+        mQuotesEditText.setText("Here are all the available Quotes: " + Quotes);
+
+
     }
 }
