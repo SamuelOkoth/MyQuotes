c13c2a3a4fd60f808e3ef01ad8b9fcdf41b4e59a
SamuelOkoth <okothsamuel78@gmail.com>
2019-09-02 11:05:18 +0300

    created OkHttpClient client to create and send our request.

--- app/src/main/java/QuoteSrvice.java
+++ app/src/main/java/QuoteSrvice.java
@@ -1,9 +1,13 @@
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+
 public class QuoteSrvice {
 
 
 
-        public static void findRestaurants(String Quotes, Callback callback) {
+        public static void findQuotes(String Quotes, Callback callback) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .build();
 
     }
 }
