330f59aee3708ea4071ed2164f7edb074994c87d
SamuelOkoth <okothsamuel78@gmail.com>
2019-09-02 11:06:52 +0300

    created OkHttpClient client to create and send our request with the base url and the token because the systematic way in gradle.properties brought errors

--- app/src/main/java/QuoteSrvice.java
+++ app/src/main/java/QuoteSrvice.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
+
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class QuoteSrvice {
 
 
 
-        public static void findQuotes(String Quotes, Callback callback) {
-            OkHttpClient client = new OkHttpClient.Builder()
+        public static void findQuotes(String Quotes, Callback callback) throws IOException {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("https://150000-quotes.p.rapidapi.com/keyword/Life")
+                    .get()
+                    .addHeader("x-rapidapi-host", "150000-quotes.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", "77721d23f1msh474eb6b7ee8bfb6p17f285jsnc5b2bccec497")
                     .build();
 
+            Response response = client.newCall(request).execute();
     }
 }
