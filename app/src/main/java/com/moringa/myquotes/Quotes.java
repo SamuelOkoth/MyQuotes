b0ccf4fc1eb390a5fc418f7b6d4604492ab99a06
SamuelOkoth <okothsamuel78@gmail.com>
2019-09-02 13:15:38 +0300

    added the seters

--- app/src/main/java/com/moringa/myquotes/Quotes.java
+++ app/src/main/java/com/moringa/myquotes/Quotes.java
@@ -1,4 +1,17 @@
 package com.moringa.myquotes;
 
 public class Quotes {
+    private String mQuote;
+    private String mAuthor;
+    private String mImageUrl;
+
+
+
+    public Quotes(String Quote, String Author, String mImageUrl){
+
+
+
+        this.mQuote
+    }
+
 }
