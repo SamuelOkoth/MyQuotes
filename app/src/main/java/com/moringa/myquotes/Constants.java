e5c66f73eb97bbd8c3eec36bdedcde5a22205b6a
SamuelOkoth <okothsamuel78@gmail.com>
2019-09-09 07:30:49 +0300

    refactored code in activiyt_main

--- app/src/main/java/com/moringa/myquotes/Constants.java
+++ app/src/main/java/com/moringa/myquotes/Constants.java
@@ -3,8 +3,13 @@ package com.moringa.myquotes;
 import com.moringa.myquotes.BuildConfig;
 
 public class Constants {
-   public static final String favqs_KEY  = BuildConfig.favqs_KEY;
-    public static final String favqs_BASE_URL = "https://favqs.com/";
-    public static final String favqs_KEY_QUERY_PARAMETER = "quotes";
+//   public static final String favqs_KEY  = BuildConfig.favqs_KEY;
+//    public static final String favqs_BASE_URL = "https://favqs.com/";
+//    public static final String favqs_KEY_QUERY_PARAMETER = "quotes";
+
+
+    public static final String QUOTE_BASE_URL = "http://quotes.stormconsultancy.co.uk/popular.json";
+    public static final String QUOTE_AUTHOR_QUERY_PARAMETER = "id";
+    public static final String PREFERENCES_AUTHOR_KEY = "author";
 
 }
